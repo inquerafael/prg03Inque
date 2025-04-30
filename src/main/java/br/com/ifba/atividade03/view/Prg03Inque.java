7804725fbfd5060a8282984b7dd8803ba527d06d
inquerafael <playerong@gmail.com>
2025-04-30 18:20:10 -0300

    criado pacotes para jframe / imagens do projeto

---
+++ src/main/java/br/com/ifba/atividade03/view/Prg03Inque.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.atividade03.view;
+
+/**
+ *
+ * @author inque
+ */
+public class Prg03Inque {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
